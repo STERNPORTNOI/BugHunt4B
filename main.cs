221613890897f2b3643b48f963c039aae980f28c
Ignacio Pardo <65306107+IgnacioPardo@users.noreply.github.com>
2022-03-18 11:16:11 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,45 @@
+/*
+BUG HUNT 4D
+
+El programa resuelve lo siguiente:
+	
+	El curso comienza con nota 0;
+	Cada alumno ingresa su nota mas reciente, 
+	la cual vale una fraccion de la cantidad de alumnos del curso, 
+	y se le suma a la nota total del curso.
+	
+	En cuanto la nota del curso alcanza 6 o más, el programa finaliza.
+	
+Por cada BUG encontrado, el grupo debe documentarlo en privado 
+y pasar al pizarron a anotarse su punto.
+
+El primer grupo en alcanzar el puntaje correcto y lograr correr el programa, gana.
+
+*/
+
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int notaCurso
+		Float cantidadAlumnos;
+		
+		Console.write('Ingrese nota de alumno');
+		console,Readline(cantidadAlumnos);
+		
+		while (notaCurso < 6){
+			notaAlumno += Console.Read();
+			Console.writeline('Ingrese nota de alumno');
+			notaPonderada = notaAlumno // cantidadAlumnos;
+		
+			notaCurso += notaAlumnos:
+			i++;
+		
+		Console.ReadLine("Ganaste");
+
+
+
+
+
+  }
+}
